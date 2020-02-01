e96b01d8b802a78bd577fe5505bd5d54e76c3753
Alkatraz <anuragbmore@gmail.com>
2020-02-01 16:48:50 +0530

    hello world evolved into file cruder

--- index.js
+++ index.js
@@ -32,3 +32,51 @@ console.log("I am Main");
 //   shell.openItem("./electron.png");
 //   shell.openExternal("http://electron.atom.io");
 // });
+
+const fs = require("fs");
+const path = require("path");
+
+btnRead = document.getElementById("btnRead");
+
+btnCreate = document.getElementById("btnCreate");
+btnDelete = document.getElementById("btnDelete");
+fileName = document.getElementById("fileName");
+fileContents = document.getElementById("fileContents");
+
+let pathName = path.join(__dirname, "Files");
+
+btnCreate.addEventListener("click", function() {
+  let file = path.join(pathName, fileName.value);
+  let contents = fileContents.value;
+  fs.writeFile(file, contents, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The file was created");
+  });
+});
+
+btnRead.addEventListener("click", function() {
+  let file = path.join(pathName, fileName.value);
+
+  fs.readFile(file, function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    fileContents.value = data;
+    console.log("The file was read");
+  });
+});
+
+btnDelete.addEventListener("click", function() {
+  let file = path.join(pathName, fileName.value);
+
+  fs.unlink(file, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    fileName.value = "";
+    fileContents.value = "";
+    console.log("The file was deleted");
+  });
+});
